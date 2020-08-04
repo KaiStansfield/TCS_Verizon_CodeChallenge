0457b3a7e8e4d75c44e53dcad8ecf37c06971176
Kai Stansfield <38113599+KaiStansfield@users.noreply.github.com>
2020-08-04 12:40:24 -0700

    Update GenerateRandomNumbers.java

--- GenerateRandomNumbers.java
+++ GenerateRandomNumbers.java
@@ -3,25 +3,29 @@ import java.util.ArrayList;
 
 public class GenerateRandomNumbers{
 
-     public static void main(String []args){
+    public static void main(String []args){
         generateRandomNumbers(50);
-     }
-     
-     void generateRandomNumbers(int n) {
-         if (n > 0) {
-             List<Integer> list = new ArrayList<Integer>();
-             for (int i = 1; i <= n; i++) {
-                 list.add(i);
-             }
-             while (list.size() > 0) {
-                 double intervalValue = 1.0/list.size();
-                 double randomValue = Math.random();
-                 int idx = (int)Math.floor(randomVal/floorValue);
-                 System.out.println(list.get(idx));
-                 list.remove(idx);
-             }
-         } else {
-             System.out.println("number should be greater than 0");
-         }
-     }
+    }
+
+    void generateRandomNumbers(int n) {
+        if (n > 0) {
+            //populate list with values [1..n]
+            List<Integer> list = new ArrayList<Integer>();
+            for (int i = 1; i <= n; i++) {
+                list.add(i);
+            }
+ 
+            while (list.size() > 0) {
+                //interval to determine which bucket the randomValue will fall in
+                double intervalValue = 1.0/list.size();
+                double randomValue = Math.random();
+                //floored to produce a correct index
+                int idx = (int)Math.floor(randomVal/floorValue);
+                System.out.println(list.get(idx));
+                list.remove(idx);
+            }
+        } else {
+            System.out.println("number should be greater than 0");
+        }
+    }
 }
